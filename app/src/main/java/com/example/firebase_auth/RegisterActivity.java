2317780c278f71ea0ff169792eac6850b3b545eb
Chromicle <ajayprabhakar369@gmail.com>
2019-07-01 17:58:16 +0530

    Firebase Added user

--- app/src/main/java/com/example/firebase_auth/RegisterActivity.java
+++ app/src/main/java/com/example/firebase_auth/RegisterActivity.java
@@ -1,25 +1,103 @@
 package com.example.firebase_auth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextView registerText;
+    FirebaseAuth mAuth;
+    EditText editText_email,editText_password;
+    Button button_register;
+    ProgressBar progressBar_signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        button_register =findViewById(R.id.button_register);
+        editText_email = findViewById(R.id.text_email);
+        editText_password = findViewById(R.id.edit_text_password);
+        progressBar_signup =findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
 
 
     }
 
     public void registerUser(){
 
+        String email = editText_email.getText().toString().trim();
+        String password = editText_password.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editText_email.setError("Email should be provided");
+            editText_email.requestFocus();
+        }
+
+        if (!isEmailValid(email)){
+            editText_email.setError("Correct email should be provided");
+            editText_email.requestFocus();
+        }
+
+        if(password.isEmpty()){
+            editText_password.setError("Password should be provided");
+            editText_password.requestFocus();
+        }
+
+        if (password.length() < 6){
+            editText_password.setError("Minimum lenght of password is 6");
+            editText_password.requestFocus();
+        }
+
+        progressBar_signup.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar_signup.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Successfully registered",Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    Toast.makeText(getApplicationContext(),"Cannot Registered user",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
 }
