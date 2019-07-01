cdf02c19160c09bb7ccd9a6534eca3d145231ddd
Chromicle <ajayprabhakar369@gmail.com>
2019-07-01 20:50:49 +0530

    Addeed Image trought Intent

--- app/src/main/java/com/example/firebase_auth/ProfileActivity.java
+++ app/src/main/java/com/example/firebase_auth/ProfileActivity.java
@@ -1,14 +1,75 @@
 package com.example.firebase_auth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_IAMGE = 101;
+    ImageView profile_pic;
+    EditText profile_name;
+    Button button_saveChanges;
+    Uri uri_profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profille);
+
+        profile_pic = findViewById(R.id.image_view);
+        profile_name = findViewById(R.id.edit_text_name);
+        button_saveChanges = findViewById(R.id.button_save);
+
+        button_saveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        profile_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageChooser();
+
+            }
+        });
+    }
+
+    public void ImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Choose Image"), CHOOSE_IAMGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+            uri_profileImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri_profileImage);
+                profile_pic.setImageBitmap(bitmap);
+                Toast.makeText(getApplicationContext(),"Succesfull",Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(),"Some thing Wrong",Toast.LENGTH_SHORT).show();
+            }
+
+
+
     }
 }
